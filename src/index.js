d414918e07d2704362473299ae92aafcd7a33703
Liu Chenchen <liucc0609@hotmail.com>
2017-02-21 20:42:33 -0800

    file structure

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+var utility = require('./utility');
+
+console.log(utility.validate.isStr('sss'));
+console.log(utility.validate.isStr(1));
---
+++ src/utility/validate.js
@@ -0,0 +1,16 @@
+
+exports.isStr = function (val) {
+    return typeof val === 'string';
+}
+
+exports.isObj = function (val) {
+    return typeof val === 'object';
+}
+
+exports.isNum = function (val) {
+    return typeof val === 'number';
+}
+
+exports.isBool = function (val) {
+    return typeof val === 'boolean';
+}
