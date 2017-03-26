22fb8dbe91cb0ac22568bcd9bb838872e51c9151
刘臣辰 <liucc0609@hotmail.com>
2017-03-26 21:22:09 +0800

    reset file

--- src/task/dispatcher/reset.js
+++ src/task/dispatcher/reset.js
@@ -4,7 +4,9 @@ var syncdateCol = require('./updateStockData/db').syncdateCol;
 var simulate = require('./simulate/db');
 var simulateCol = simulate.simulateCol;
 var lastSimDateCol = simulate.lastSimDateCol;
+var azure = require('../../utility').azureStorage;
+var container = require('../../config').stockdataContainer;
 
-Promise.all([taskCol.remove(), syncdateCol.remove(), simulateCol.remove(), lastSimDateCol.remove()]).then(() => {
+Promise.all([taskCol.remove(), syncdateCol.remove(), simulateCol.remove(), lastSimDateCol.remove(), azure.deleteContainer(container)]).then(() => {
     console.log('done');
 })
