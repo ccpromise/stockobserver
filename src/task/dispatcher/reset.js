9f1e547f4099706165a22998a4a7ca058c29d751
刘臣辰 <liucc0609@hotmail.com>
2017-03-28 14:20:13 +0800

    reset database

--- src/task/dispatcher/reset.js
+++ src/task/dispatcher/reset.js
@@ -2,11 +2,15 @@
 var taskCol = require('./taskManager/db').taskCol;
 var syncdateCol = require('./updateStockData/db').syncdateCol;
 var simulate = require('./simulate/db');
+var trade = require('./trade/db').tradeplanCol;
 var simulateCol = simulate.simulateCol;
 var lastSimDateCol = simulate.lastSimDateCol;
-var azure = require('../../utility').azureStorage;
-var container = require('../../config').stockdataContainer;
+var config = require('../../config');
+var azure = require('../../utility').azureStorage(config.azureUsr);
+var container = config.stockdataContainer;
+var tradePlan = require('../../strategy/tradeplan');
 
-Promise.all([taskCol.remove(), syncdateCol.remove(), simulateCol.remove(), lastSimDateCol.remove(), azure.deleteContainer(container)]).then(() => {
+
+Promise.all([trade.remove().then(() => { return trade.insertMany(Object.values(tradePlan)); }), taskCol.remove(), syncdateCol.remove(), simulateCol.remove(), lastSimDateCol.remove(), azure.deleteContainer(container)]).then(() => {
     console.log('done');
-})
+});
