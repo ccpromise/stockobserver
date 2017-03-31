770f4ec76aa0a4bc4384c7ff4fab31e7e6032bb0
刘臣辰 <liucc0609@hotmail.com>
2017-03-31 15:20:12 +0800

    edit

--- src/dataPvd/makeDataPvd.js
+++ src/dataPvd/makeDataPvd.js
@@ -1,79 +1,74 @@
 
-var DataPvds = require('./pvdClass');
-var utility = require('../utility');
-var validate = utility.validate;
-var existObj = {};
-var cachedPvdList = {'ma': true, 'std': true, 'boll': true, 'ema': true, 'macd': true}
-var pvdList = {
-    'ma': DataPvds.MADataPvd,
-    'std': DataPvds.StdDataPvd,
-    'boll': DataPvds.BollDataPvd,
-    'ema': DataPvds.EMADataPvd,
-    'macd': DataPvds.MACDDataPvd,
-    'add': DataPvds.AddDataPvd,
-    'div': DataPvds.DivDataPvd,
-    'mul': DataPvds.MulDataPvd,
-    'sub': DataPvds.SubDataPvd,
-    'const': DataPvds.ConstDataPvd,
-    'offset': DataPvds.OffsetDataPvd,
-    'end': DataPvds.EndDataPvd,
-    'and': DataPvds.AndDataPvd,
-    'gt': DataPvds.GtDataPvd
+/**
+ * generate data pvd instance according to a literal description like { type: 'end', pack: '000001.xshe'}
+ * each data pvd class provides interface: {
+ * checkParams: function(ldp) {},
+ * pvdID: function(ldp) {},
+ * makePvd: function(ldp) {},
+ * }
+ * this module is to assign checkParams/pvdID/makePvd tasks according to ldp's type,
+ * and to make cache for compute intensive pvd.
+ * usage sample:
+ * var pvdPromise = makeDataPvd({ type: 'ma', pack: { pvd: { type: 'end', pack: '000001.xshe'}, N: 10}})
+ * generate a moving average data provider, which use end-data of stock '000001.xshe' and average window size is 10.
+ */
+const validate = require('../utility').validate;
+const cachedPvdList = {'ma': true, 'std': true, 'boll': true, 'ema': true, 'macd': true}
+const DataPvdLib = require('./pvdClass');
+const pvdMap = {
+    'ma': DataPvdLib.MADataPvd,
+    'std': DataPvdLib.StdDataPvd,
+    'boll': DataPvdLib.BollDataPvd,
+    'ema': DataPvdLib.EMADataPvd,
+    'macd': DataPvdLib.MACDDataPvd,
+    'add': DataPvdLib.AddDataPvd,
+    'div': DataPvdLib.DivDataPvd,
+    'mul': DataPvdLib.MulDataPvd,
+    'sub': DataPvdLib.SubDataPvd,
+    'const': DataPvdLib.ConstDataPvd,
+    'offset': DataPvdLib.OffsetDataPvd,
+    'end': DataPvdLib.EndDataPvd,
+    'and': DataPvdLib.AndDataPvd,
+    'gt': DataPvdLib.GtDataPvd
 }
 
-// ldp: {'type': , 'pack': {}}
+/**
+ * check if liter dp is valid:
+ * ldp is an instance of base class DataPvd,
+ * or ldp is in { type: , pack: } format
+ */
 function checkldp(ldp) {
-    if(ldp instanceof DataPvds.DataPvd) return true;
-    if(!(validate.isObj(ldp) && ldp.type in pvdList)) {
+    if(ldp instanceof DataPvdLib.DataPvd) return true;
+    if(!(validate.isObj(ldp) && ldp.type in pvdMap)) {
         return false;
     }
-    return pvdList[ldp.type].checkParams(ldp.pack);
+    return pvdMap[ldp.type].checkParams(ldp.pack);
 }
 
+/**
+ * generate id using liter dp
+ */
 function pvdID(ldp) {
     if(!checkldp(ldp)) throw new Error('invalid literal dp');
-    if(ldp instanceof DataPvds.DataPvd) return ldp.id;
-    return pvdList[ldp.type].pvdID(ldp.pack);
+    if(ldp instanceof DataPvdLib.DataPvd) return ldp.id;
+    return pvdMap[ldp.type].pvdID(ldp.pack);
 }
 
+/**
+ * generate date pvd according to ldp.
+ */
 var pvdPromiseMap = {};
 function makePvd(ldp) {
     return Promise.resolve().then(() => {
-        if(ldp instanceof DataPvds.DataPvd) return ldp;
+        if(ldp instanceof DataPvdLib.DataPvd) return ldp;
         if(!checkldp(ldp)) throw new Error('invalid literal dp');
         var id = pvdID(ldp);
         if(id in pvdPromiseMap) return pvdPromiseMap[id];
-        var promise = pvdList[ldp.type].makePvd(ldp.pack, id);
+        var promise = pvdMap[ldp.type].makePvd(ldp.pack, id);
         if(ldp.type in cachedPvdList) pvdPromiseMap[id] = promise;
         return promise;
     })
 }
-    /*
-    return new Promise((resolve, reject) => {
-        try {
-            if(validate.isDataPvd(ldp)) resolve(ldp);
-            else if(!checkldp(ldp)) reject(new Error('invaid literal dp'));
-            else {
-                var id = pvdID(ldp);
-                if(id in existObj) resolve(existObj[id]);
-                else if(id in pendingObj) resolve(pendingObj[id]);
-                else {
-                    var promise = pvdList[ldp.type].makePvd(ldp.pack, id);
-                    promise.then((createdObj) => {
-                        if(ldp.type in cachedPvdList) existObj[id] = createdObj;
-                        delete pendingObj[id];
-                        resolve(createdObj);
-                    }).catch(reject);
-                    pendingObj[id] = promise;
-                }
-            }
-        }
-        catch(err) {
-            reject(err);
-        }
-    });
-    */
-
 
 exports.checkldp= checkldp;
 exports.pvdID = pvdID;
